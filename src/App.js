d89d001b56d2d776ef9134d36c946f0ac2929d3a
EugeneAlimov <zeka_ali@i.ua>
2022-12-05 00:05:09 +0100

    add Main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // import logo from './logo.svg';
 import './App.css';
 import LeftAbout from './Components/LeftAbout';
+import Main from './Components/Main';
 
 function App() {
   return (
     <div className="App">
       <header className="Resume-header">
         <LeftAbout />
+        <Main />
       </header>
     </div>
   );
--- src/Components/LeftAbout.jsx
+++ src/Components/LeftAbout.jsx
@@ -1,18 +1,51 @@
 import React from "react";
 import className from "./style/LeftAbout.module.css";
-import photo from '../media/photo.jpg'
+import photo from "../media/photo.jpg";
 
 const LeftAbout = () => {
   return (
     <div className={className.Wrapper}>
-      <img className={className.Photo} src={photo} alt='fhoto' width={'200px'}></img>
+      <img className={className.Photo} src={photo} alt="fhoto" width={"200px"}></img>
       <div className={className.ContactsWrapper}>
-        <div>Contacts</div>
-        <div>PHONE:</div>
-        <a href="tel:+380632154288">+38-063-215-42-88</a>
-        <div>e-mail:</div>
-        <a href="mailto:yevhen.alimov@gmail.com">yevhen.alimov@gmail.com</a>
-
+        <h3 className={className.ContactsTitle}>Contacts</h3>
+        <div className={className.Contact}>
+          <div className={className.contactType}>Phone:</div>
+          <a className={className.ContactLink} href="tel:+380632154288">
+            +38-063-215-42-88
+          </a>
+        </div>
+        <div className={className.Contact}>
+          <div className={className.contactType}>e-mail:</div>
+          <a className={className.ContactLink} href="mailto:yevhen.alimov@gmail.com">
+            yevhen.alimov@gmail.com
+          </a>
+        </div>
+        <section className={className.Section}>
+          <h3 className={className.SectionTitle}>Language</h3>
+          <ul className={className.SectionList}>
+            <li className={className.SectionListItem}>
+              <span>English - pre intermediate</span>
+            </li>
+            <li className={className.SectionListItem}>
+              <span>Ukrainian, Russian - native</span>
+            </li>
+          </ul>
+        </section>
+        <section className={className.Section}>
+          <h3 className={className.SectionTitle}>Social</h3>
+          <ul className={className.SectionList}>
+            <li className={className.SectionListItem}>
+              <a className={className.SocialLink} href="https://github.com/EugeneAlimov">
+                GitHub
+              </a>
+            </li>
+            <li className={className.SectionListItem}>
+              <a className={className.SocialLink} href="https://www.linkedin.com/in/yevhen-alimov-514568208/">
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </section>
       </div>
     </div>
   );
