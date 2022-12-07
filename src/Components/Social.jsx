b8a1715dc29859aa25616546224885de106860a4
EugeneAlimov <zeka_ali@i.ua>
2022-12-07 07:08:16 +0100

    refactoring

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -3,9 +3,10 @@ import React from "react";
 import Stack from "@mui/material/Stack";
 import Link from "@mui/material/Link";
 import GitHub from "@mui/icons-material/GitHub";
-import Telegram from "@mui/icons-material/Telegram";
 import WhatsApp from "@mui/icons-material/WhatsApp";
+import LinkedIn from "../LinkedIn";
 import Viber from "../Viber";
+import Telegram from "../Telegram";
 
 const Social = () => {
   return (
@@ -20,16 +21,19 @@ const Social = () => {
         spacing={5}
       >
         <Link href="https://github.com/EugeneAlimov">
-          <GitHub sx={{ color: "#000", textShadow: 0, fontSize: 90 }} />
+          <GitHub sx={{ filter: "drop-shadow(4px 3px 4px rgb(0 0 0 / 0.4))", color: "#000", textShadow: 0, fontSize: 90 }} />
+        </Link>
+        <Link href="https://www.linkedin.com/in/yevhen-alimov-514568208/">
+          <LinkedIn className="linkedin-social" />
         </Link>
         <Link href="https://t.me/eugene1131">
-          <Telegram sx={{ color: "#1296c9", fontSize: 90 }} />
+          <Telegram className="telegram-social" />
         </Link>
         <Link href="https://wa.me/380632154288">
-          <WhatsApp sx={{ color: "#30b545", fontSize: 90 }} />
+          <WhatsApp sx={{ filter: "drop-shadow(4px 3px 4px rgb(0 0 0 / 0.4))", color: "#30b545", fontSize: 90 }} />
         </Link>
         <Link href="viber://chat?number=+380632154288">
-          <Viber className="Viber-social" />
+          <Viber className="viber-social" />
         </Link>
       </Stack>
     </Box>
