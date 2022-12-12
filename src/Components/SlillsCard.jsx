2632264170cabf5034051204c69cbc631f1e044e
EugeneAlimov <zeka_ali@i.ua>
2022-12-13 00:36:08 +0100

    refactioring

--- src/Components/SlillsCard.jsx
+++ src/Components/SlillsCard.jsx
@@ -4,16 +4,7 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Divider from "@mui/material/Divider";
-import { styled } from "@mui/material/styles";
 import SkillsList from "./Skillslist";
-
-const ContentBox = styled(Box)(() => ({
-  "@media print": {
-    marginTop: 0,
-    paddingTop: 0,
-    paddingBottom: 0,
-  },
-}));
 
 const SkillsCard = ({ matchesDownSm, content, title, icon }) => {
   return (
@@ -32,7 +23,19 @@ const SkillsCard = ({ matchesDownSm, content, title, icon }) => {
           "0px 2px 1px -1px rgba(0,0,0,0.2),0px 1px 1px 0px rgba(0,0,0,0.14),0px 1px 3px 0px rgba(0,0,0,0.12)",
       }}
     >
-      <ContentBox ml={3} mr={3} paddingTop={5} paddingBottom={3}>
+      <Box
+        sx={{
+          "@media print": {
+            marginTop: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+          },
+        }}
+        ml={3}
+        mr={3}
+        paddingTop={5}
+        paddingBottom={3}
+      >
         <Box>
           <Typography color={"#99bf34"} fontWeight={700} variant="h4" component="h3">
             {title}
@@ -48,7 +51,7 @@ const SkillsCard = ({ matchesDownSm, content, title, icon }) => {
             return <SkillsList matchesDownSm={matchesDownSm} icon={icon} {...el} key={key} />;
           })}
         </Stack>
-      </ContentBox>
+      </Box>
     </Box>
   );
 };
